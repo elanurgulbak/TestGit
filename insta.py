5fd38a048cd53a64e6f018128a327f3bd1f25675
elanurgulbak <46396063+elanurgulbak@users.noreply.github.com>
2019-04-30 23:45:56 +0300

    Update insta.py

--- insta.py
+++ insta.py
@@ -8,14 +8,6 @@ time.sleep(10)
 giris_yap = browser.find_element_by_xpath("/html/body/span/section/main/"
                                         "article/div[2]/div[2]/p/a")
 giris_yap.click()
-time.sleep(2)
-username=browser.find_element_by_name("username")
-password=browser.find_element_by_name("password")
-username.send_keys("xxxxx")
-password.send_keys("xxxxx")
-time.sleep(5)
-giris_yap2 = browser.find_element_by_xpath("/html/body/span/section/main/div/article/"
-                                         "div/div[1]/div/form/div[3]")
-giris_yap2.click()
+
 time.sleep(10)
 browser.close()
